bb6e66bc79515bcd57c1324b286f05699838a4c6
Ty <87083014+x-typo@users.noreply.github.com>
2026-04-20 15:30:57 -0600

    Scaffold Xcode project with gitignore (Task 1 baseline)

---
+++ Gaze/Gaze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gaze
+//
+//  Created by Ty IllI on 4/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gaze/Gaze/GazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GazeApp.swift
+//  Gaze
+//
+//  Created by Ty IllI on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
